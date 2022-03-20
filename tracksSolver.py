c45b83c043cd9bce57988eedc73fc6d83e4ac412
rems64 <rems64des@gmail.com>
2022-03-20 14:15:15 +0100

    Added solving with speed

--- tracksSolver.py
+++ tracksSolver.py
@@ -7,6 +7,7 @@ msg = "Tracking line solving"
 
 parser = argparse.ArgumentParser(description = msg)
 parser.add_argument("-i", "--input", help = "Input file", default="data.data")
+parser.add_argument("-o", "--output", help = "Output file", default="solved.data")
 parser.add_argument("-s", "--show", help = "Show plot results", default=False)
 parser.add_argument("-r", "--restrict", help = "Restrict number of tracks", default=0)
 parser.add_argument("-cp", "--compare", help = "Comapre the raw results with the solved ones", default=False)
@@ -39,9 +40,43 @@ def associate_tracks(frames):
                 continue
             distances = []
             for potential in potentials:
-                distances.append(np.linalg.norm((potential[0] - track[-1][0], potential[1] - track[-1][1])))
+                distances.append(np.linalg.norm((np.array(potential) - np.array(track[-1]))))
             indexOfMin = np.argmin(distances)
             track.append(potentials[indexOfMin])
+    return associated
+
+
+def associate_tracks_withSpeed(frames):
+    associated = [[] for _ in range(getNumberTracks(frames))]
+    for frame in frames:
+        potentials = []
+        for i in frame[1]:
+            # print(i)
+            potentials.append(i)
+        for track in associated:
+            if len(track)<=0:
+                track.append(potentials.pop(0))
+                continue
+            if len(track)>=2:
+                if len(potentials)<=0:
+                    break
+                # Use speed to determine best match
+                speed = np.linalg.norm(np.array(track[-1]) - np.array(track[-2]))
+                # Compute the potential speeds
+                potentials_speed = []
+                for i in potentials:
+                    potentials_speed.append(np.linalg.norm(np.array(i) - np.array(track[-1])))
+                # Find the best match
+                best_match = potentials[np.argmin(potentials_speed)]
+                track.append(best_match)
+                potentials.remove(best_match)
+            else:
+                # Fallback to closest
+                distances = []
+                for potential in potentials:
+                    distances.append(np.linalg.norm((np.array(potential) - np.array(track[-1]))))
+                indexOfMin = np.argmin(distances)
+                track.append(potentials[indexOfMin])
     return associated
 
 
@@ -53,36 +88,46 @@ def naive_associate(frames):
     return associated
 
 
-solvedTracks = associate_tracks(tracks)
+solvedTracksSpeed = associate_tracks_withSpeed(tracks)
 if args.restrict:
-    solvedTracks = solvedTracks[:int(args.restrict)]
+    solvedTracksSpeed = solvedTracksSpeed[:int(args.restrict)]
 
 if args.compare:
+    solvedTracks = associate_tracks(tracks)
     naiveTracks = naive_associate(tracks)
     if args.restrict:
+        solvedTracks = solvedTracks[:int(args.restrict)]
         naiveTracks = naiveTracks[:int(args.restrict)]
     i=0
     for i in range(len(naiveTracks)):
         nT = naiveTracks[i]
         T = solvedTracks[i]
+        Ts = solvedTracksSpeed[i]
         plt.figure()
-        plt.subplot(2, 1, 1)
+        plt.subplot(3, 1, 1)
         plt.plot([k for k in range(len(nT))], [i[0] for i in nT], 'r-')
         plt.plot([k for k in range(len(nT))], [i[1] for i in nT], 'g-')
         plt.title("Raw track " + str(i))
-        plt.subplot(2, 1, 2)
+        plt.subplot(3, 1, 2)
         plt.plot([k for k in range(len(T))], [i[0] for i in T], 'r-')
         plt.plot([k for k in range(len(T))], [i[1] for i in T], 'g-')
         plt.title("Track " + str(i))
+        plt.subplot(3, 1, 3)
+        plt.plot([k for k in range(len(Ts))], [i[0] for i in Ts], 'r-')
+        plt.plot([k for k in range(len(Ts))], [i[1] for i in Ts], 'g-')
+        plt.title("Track speed " + str(i))
         i+=1
     plt.show()
 else:
     i=0
-    for track in solvedTracks:
+    for track in solvedTracksSpeed:
         plt.figure()
         plt.plot([k for k in range(len(track))], [i[0] for i in track], 'r-')
         plt.plot([k for k in range(len(track))], [i[1] for i in track], 'g-')
         plt.title("Track " + str(i))
-        plt.show()
         i+=1
+    plt.show()
 
+if args.output:
+    with open(args.output, 'wb') as filehandle:
+        pickle.dump(solvedTracksSpeed, filehandle)
