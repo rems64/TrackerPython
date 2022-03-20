72c3297dc7bec2b398338176f08b78c98f3766eb
rems64 <rems64des@gmail.com>
2022-03-20 13:41:26 +0100

    First approach to solve point continuity

--- fastMarkerDetect.py
+++ fastMarkerDetect.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
 import argparse
 import utils
+import pickle
+
 
 
 
@@ -13,6 +14,7 @@ parser = argparse.ArgumentParser(description = msg)
 parser.add_argument("-i", "--input", help = "Input file", default="../../Footages/Clean2/Cam1/0001-0300.mp4")
 parser.add_argument("-o", "--output", help = "Output file", default="tracked.data", required=True)
 parser.add_argument("-r", "--restrict", help = "Restrict frame count", default=100000)
+parser.add_argument("-s", "--show", help = "Show raw result", default=False)
 args = parser.parse_args()
 
 
@@ -20,45 +22,38 @@ cap = cv2.VideoCapture(args.input)
 
 tracks = []
 
-counter = 0
+frame = 0
 limit = int(args.restrict)
 
 while True:
-    if counter>limit:
+    if frame>=limit:
         break
-    counter+=1
-    ret, frame = cap.read()
+    frame+=1
+    ret, pic = cap.read()
     if ret:
-        sized = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+        sized = cv2.resize(pic, (0, 0), fx=0.5, fy=0.5)
         noise = np.random.randint(0, 100, (sized.shape[0], sized.shape[1], 3))
 
         sized = np.uint8(np.clip(sized + noise, 0, 255))
         ret, thresh = cv2.threshold(cv2.cvtColor(sized, cv2.COLOR_BGR2GRAY), 127, 255, 0)
         contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        i=0
+        detected = []
         for cnt in contours:
-            if len(tracks) <= i:
-                tracks.append([])
             M = cv2.moments(cnt)
             if M["m00"] != 0:
                 cX = int(M["m10"] / M["m00"])
                 cY = int(M["m01"] / M["m00"])
-                tracks[i].append((cX, cY))
-            i+=1
+                detected.append((cX, cY))
+        tracks.append((frame, detected))
     else:
         break
 
 
-"""
-for track in tracks:
-    plt.plot([k for k in range(len(track))], [i[0] for i in track], 'r-')
-    plt.plot([k for k in range(len(track))], [i[1] for i in track], 'g-')
-plt.show()
-"""
-
-import pickle
-
-print(tracks)
+if args.show:
+    for track in tracks:
+        plt.plot([k for k in range(len(track))], [i[0] for i in track], 'r-')
+        plt.plot([k for k in range(len(track))], [i[1] for i in track], 'g-')
+    plt.show()
 
 with open(utils.check_extension(args.output, "data"), 'wb') as filehandle:
     # store the data as binary data stream
--- tracksSolver.py
+++ tracksSolver.py
@@ -7,6 +7,9 @@ msg = "Tracking line solving"
 
 parser = argparse.ArgumentParser(description = msg)
 parser.add_argument("-i", "--input", help = "Input file", default="data.data")
+parser.add_argument("-s", "--show", help = "Show plot results", default=False)
+parser.add_argument("-r", "--restrict", help = "Restrict number of tracks", default=0)
+parser.add_argument("-cp", "--compare", help = "Comapre the raw results with the solved ones", default=False)
 args = parser.parse_args()
 
 tracks = []
@@ -14,25 +17,72 @@ tracks = []
 with open(args.input, 'rb') as filehandle:
     tracks = pickle.load(filehandle)
 
-def associate_tracks(tracks):
-    associated = [[] for _ in range(len(tracks))]
-    for frame in range(len(tracks[0])):
+def getNumberTracks(frames):
+    numberTracks = []
+    for frame in frames:
+        numberTracks.append(len(frame[1]))
+    if args.show:
+        plt.plot(numberTracks)
+        plt.show()
+    return int(np.median(numberTracks))
+
+def associate_tracks(frames):
+    associated = [[] for _ in range(getNumberTracks(frames))]
+    for frame in frames:
         potentials = []
-        for i in range(len(tracks)):
-            print(tracks[i])
-            print(len(tracks[i]))
-            print(len(tracks[0]))
-            potentials.append((tracks[i][frame]))
+        for i in frame[1]:
+            # print(i)
+            potentials.append(i)
         for track in associated:
-            track.append(potentials.pop(np.argmin([np.linalg.norm(i-j) for j in range(len(potentials))])))
+            if len(track)<=0:
+                track.append(potentials.pop(0))
+                continue
+            distances = []
+            for potential in potentials:
+                distances.append(np.linalg.norm((potential[0] - track[-1][0], potential[1] - track[-1][1])))
+            indexOfMin = np.argmin(distances)
+            track.append(potentials[indexOfMin])
     return associated
 
 
-aTracks = associate_tracks(tracks)
+def naive_associate(frames):
+    associated = [[] for _ in range(getNumberTracks(frames))]
+    for frame in frames:
+        for i in range(len(frame[1])):
+            associated[i].append(frame[1][i])
+    return associated
 
-for track in aTracks:
-    plt.figure()
-    plt.plot([k for k in range(len(track))], [i[0] for i in track], 'r-')
-    plt.plot([k for k in range(len(track))], [i[1] for i in track], 'g-')
-plt.show()
+
+solvedTracks = associate_tracks(tracks)
+if args.restrict:
+    solvedTracks = solvedTracks[:int(args.restrict)]
+
+if args.compare:
+    naiveTracks = naive_associate(tracks)
+    if args.restrict:
+        naiveTracks = naiveTracks[:int(args.restrict)]
+    i=0
+    for i in range(len(naiveTracks)):
+        nT = naiveTracks[i]
+        T = solvedTracks[i]
+        plt.figure()
+        plt.subplot(2, 1, 1)
+        plt.plot([k for k in range(len(nT))], [i[0] for i in nT], 'r-')
+        plt.plot([k for k in range(len(nT))], [i[1] for i in nT], 'g-')
+        plt.title("Raw track " + str(i))
+        plt.subplot(2, 1, 2)
+        plt.plot([k for k in range(len(T))], [i[0] for i in T], 'r-')
+        plt.plot([k for k in range(len(T))], [i[1] for i in T], 'g-')
+        plt.title("Track " + str(i))
+        i+=1
+    plt.show()
+else:
+    i=0
+    for track in solvedTracks:
+        plt.figure()
+        plt.plot([k for k in range(len(track))], [i[0] for i in track], 'r-')
+        plt.plot([k for k in range(len(track))], [i[1] for i in track], 'g-')
+        plt.title("Track " + str(i))
+        plt.show()
+        i+=1
 
